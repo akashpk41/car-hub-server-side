311ad348fa9262364aa3be51c2b038dd06f42c68
akashpk41 <akashpk84100@gmail.com>
2022-05-01 11:50:08 +0600

    mongodb database connected successfully

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 // dependencies
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-     res.send("Car Hub Server Is Running...")
-})
+app.get("/", (req, res) => {
+  res.send("Car Hub Server Is Running...");
+});
 
+// ? -------------------- MongoDB -----------------------
 
-app.listen(port,()=>{
-     console.log('Server Is Running On Port : ', port);
-})
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q4llj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const carCollection = client.db("carHub").collection("car");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+run();
+// ? -------------------- MongoDB -----------------------
+
+app.listen(port, () => {
+  console.log("Server Is Running On Port : ", port);
+});
